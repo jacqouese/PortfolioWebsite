acb70cb002287fa75c3f0a59f99223a121e46ddd
Jacqouese <jakub.czaja003@gmail.com>
2022-04-02 12:42:18 +0200

    blob behind project img

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,60 +1,85 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
-import projects from '../data/projects'
-
+import projects from '../data/projects';
+import { SvgBlob } from 'react-svg-blob';
 
 function ProjectsSection({ offsetY }) {
-
-  
   return (
     <div className="projects-section">
-        <motion.h1 
-         className="h1-outline"
-         style={{ transform: `translateX(-${offsetY * 0.2 - 200}px)` }}
+      <motion.h1
+        className="h1-outline"
+        style={{ transform: `translateX(-${offsetY * 0.2 - 200}px)` }}
+      >
+        My highlighted projects
+      </motion.h1>
+      {projects.map((project) => (
+        <motion.div
+          className="project-container"
+          style={{ backgroundColor: project.colors.primary }}
+          transition={{ duration: 1 }}
+          initial={{ translateY: 100, opacity: 0 }}
+          whileInView={{ translateY: 0, opacity: 1 }}
         >
-            My highlighted projects
-        </motion.h1>
-        {projects.map(project =>
-            <motion.div
-             className="project-container"
-             style={{ backgroundColor: project.colors.primary }}
-             transition={{ duration: 1 }}
-             initial={{ translateY: 100, opacity: 0 }}
-             whileInView={{ translateY: 0, opacity: 1 }}
-            >
-            <motion.div 
-             className="project-container-text"
-             transition={{ duration: 0.9, delay: 0.1 }}
-             initial={{ translateY: -100, opacity: 0 }}
-             whileInView={{ translateY: 0, opacity: 1 }}
-            >
-                <h1>{project.name}</h1>
-                <p>{project.title}</p>
-                <p className="font-smaller" dangerouslySetInnerHTML={{ __html: project.subtitle }}></p>
-                <div className="project-technologies-container">
-                    {project.technologies.map(technology =>
-                        <span style={{backgroundColor: project.colors.lighter}}>{technology}</span>
-                    )}
-                </div>
-               <div>
-                    <a href="https://www.github.com" className="project-button" style={{backgroundColor: project.colors.darker}}>Detailed info</a>
-                    <a href={project.url} target="_blank" className="project-button" style={{backgroundColor: project.colors.darker}}>source code</a>
-               </div>
-            </motion.div>
-            <div className="project-container-image">
-            
-                <motion.img
-                  src={project.image}
-                  alt=""
-                  transition={{ duration: 0.9, delay: 0.1 }}
-                  initial={{ opacity: 0, translateY: 100 }}
-                  whileInView={{ opacity: 1, translateY: 0 }}/>
+          <motion.div
+            className="project-container-text"
+            transition={{ duration: 0.9, delay: 0.1 }}
+            initial={{ translateY: -100, opacity: 0 }}
+            whileInView={{ translateY: 0, opacity: 1 }}
+          >
+            <h1>{project.name}</h1>
+            <p>{project.title}</p>
+            <p
+              className="font-smaller"
+              dangerouslySetInnerHTML={{ __html: project.subtitle }}
+            ></p>
+            <div className="project-technologies-container">
+              {project.technologies.map((technology) => (
+                <span style={{ backgroundColor: project.colors.lighter }}>
+                  {technology}
+                </span>
+              ))}
             </div>
-        </motion.div>          
-        )}
+            <div>
+              <a
+                href="https://www.github.com"
+                className="project-button"
+                style={{ backgroundColor: project.colors.darker }}
+              >
+                Detailed info
+              </a>
+              <a
+                href={project.url}
+                target="_blank"
+                className="project-button"
+                style={{ backgroundColor: project.colors.darker }}
+              >
+                source code
+              </a>
+            </div>
+          </motion.div>
+          <div className="project-container-image">
+            <motion.img
+              src={project.image}
+              alt=""
+              transition={{ duration: 0.9, delay: 0.1 }}
+              initial={{ opacity: 0, translateY: 100 }}
+              whileInView={{ opacity: 1, translateY: 0 }}
+              className="project-img"
+            />
+
+            <div className="blob-img">
+              <SvgBlob
+                variant="solid"
+                color={project.colors.lighter}
+                shapeProps={{ size: 14, growth: 10, edges: 17 }}
+              />
+            </div>
+          </div>
+        </motion.div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProjectsSection
+export default ProjectsSection;
--- src/components/TechnologiesSection.js
+++ src/components/TechnologiesSection.js
@@ -4,9 +4,8 @@ import { motion } from 'framer-motion';
 import skills from '../data/skills';
 
 function TechnologiesSection({ offsetY }) {
-
   return (
-    <div className="TechnologiesSection">
+    <div className="technologies-section">
       <motion.h1
         className="h1-outline"
         style={{ transform: `translateX(${offsetY * 0.2 - 200}px)` }}
@@ -24,9 +23,9 @@ function TechnologiesSection({ offsetY }) {
           <div className="skills-column">
             <div className="skill-parent">
               <motion.div
-               transition={{ duration: 0.5, delay: 0.2 }}
-               initial={{ translateY: -80, opacity: 0 }}
-               whileInView={{ translateY: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+                initial={{ translateY: -80, opacity: 0 }}
+                whileInView={{ translateY: 0, opacity: 1 }}
               >
                 <i class={skill.icon}></i>
                 <h3>{skill.name}</h3>
